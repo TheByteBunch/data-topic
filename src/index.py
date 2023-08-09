21392e121a1d52c262f517652568f2ee8ad3cc0c
cc <31040856+CCleanerShot@users.noreply.github.com>
2023-08-09 08:08:21 -0500

    im brain fried i cannot even think rn

--- src/index.py
+++ src/index.py
@@ -2,23 +2,44 @@
 import psycopg2
 import local_youtube_api
 import local_psql
-
-def extract_channel_info(channel_data):
+import input
+def extract_channel_info(channel_name, channel_data):
     subscriber_count = channel_data['items'][0]['statistics']['subscriberCount']
     view_count = channel_data['items'][0]['statistics']['viewCount']
     video_count = channel_data['items'][0]['statistics']['videoCount']
-    return subscriber_count, view_count, video_count
+    return channel_name, subscriber_count, view_count, video_count
 
-channels = ['Google','Schafer5']
-for channel in channels:
-    result = local_youtube_api.request_channel(channel)
-    data = extract_channel_info(result)
-    print(data)
-    # psql.query_sql("query here")
+print_result = input.get_input()
+print(print_result)
+result = local_youtube_api.request_channel(print_result)
+data = extract_channel_info(print_result, result)
+channel_name = str(data[0])
+subscriber_count = data[1]
+view_count = data[2]
+video_count = data[3]
+message = f"INSERT INTO channels (name, subscribers, views, video_count) VALUES ('{channel_name}', {subscriber_count}, {view_count}, {video_count})"
+print(message)
+local_psql.query_sql(message)
 
 
+"INSERT INTO channels (name, subscribers, views, video_count) VALUES ( X, 2, 4, 6);"
 
-#channel_id , channel_name, subscriber_count, view_count  ### sql table names
+# TERMINAL PROGRAM
+# TERMINAL PROGRAM
+# TERMINAL PROGRAM
 
+# TOMMOROW:
+# LOOK UP ARRAYS PSQL
+# MY BRAIN IS FRIED IDK WHAT NEXT WE WILL FIGURE IT OUT TROMMOROW
 
-#INSERT INTO youtube_channels VALUES (#)
+#
+# TO-DO-LIST:
+# UPDATE THE DATABASE DAILY, FROM THE DAILY SUBSCRIBER COUNT
+# CREATE A GRAPH FROM THE DATAPOINTS OF THE DATABASE
+# LIMIT CHANNEL GRAPH VIEW LIMIT FOR USER
+
+# USER:
+# INPUT SOME CHANNELS
+# GOES INTO DATABASE
+# LEAVE FOR SOME DAYS
+# SO YOU CAN LOOK AT GRAPH AND DO SOME STATISTICS
---
+++ src/input.py
@@ -0,0 +1,3 @@
+def get_input():
+    myname = input('Name: ')
+    return(myname)
